a08f9ac5df0bf859725680e61ef884a72713b838
Queue <milain.l.ambers@gmail.com>
2017-11-22 13:01:24 +0100

    enzymes file and some try/excepts
    its a complete mess, idk where to stop
    imo, I'm about to except any error and quit at this point

--- Opdracht1/blok1_afvink6.py
+++ Opdracht1/blok1_afvink6.py
@@ -3,39 +3,22 @@
 # Datum: 16 Nov 2017
 # Versie: 3 testfase
 
-extentions = ['.fa','.fna','.fasta']
-def askUser():
-    gevonden = False
     
-    while not gevonden:
-        try:
-            isfasta = False
-            while not isfasta:
-                filename = input("I'd like a fasta filename right here, with extention:")
-                if not filename.endswith(tuple(extentions):
-                    print("wrong extention. please input a valid filename")
-                else
-            
-            file = open(filename, 'r')    
-            gevonden = True
-        except IOError:
-            print("File does not exist")
-            
-    return file
-                    
 def main(debug=False):
     """
     """
-
-    bestand = "test.fa"
-    enzym_bestand = "enzymen.txt"
+    welcome_user()
+    bestand = ask_user_for_file("FASTA filename: ")
+    enzym_bestand = ask_user_for_file("enzymes filename: ")
+    enzymes = False
+    while not enzymes:
+        enzymes = get_enzyme_data(enzym_bestand)
 
     
     headers, seqs = lees_inhoud(bestand) 
-        
     zoekwoord = input("Geef een zoekwoord op: ")
 
-    enzymes = get_enzyme_data(enzym_bestand)
+
         
     if debug: print(enzymes)
     
@@ -44,21 +27,75 @@ def main(debug=False):
             print(headers[i])
             if is_dna(seqs[i]):
                 knipt(seqs[i], enzymes)
+
                 
 
-def lees_inhoud(bestands_naam):
-    bestand = open(bestands_naam)
+
+def welcome_user():
+    """welcomes the user"""
+#!!!Question
+#   - wrote this function to remove clutter from main()
+#   - is this 'right'?
+#   - is printlines() a bad way of printing lines in between actions?
+    print("welcome")
+    printlines()
+    print("To work, I need 2 files:")
+    print("FASTA")
+    print("\tThis file should contain pure FASTA data.")
+    print("enzymes")
+    print("\tThis file should contain a list of ")
+    print("\tenzyme-names and their 'cut' location line separated.")
+    printlines()
+    
+def printlines():
+    print("--------------------------------------------------------")
+
+extentions = ['.fa','.fna','.fasta', '.txt']
+def ask_user_for_file(prompt):
+    """ask a user for a file (uses promt)
+    assuming the file is a textfile...  
+    INPUT  (optional) a prompt to display
+    OUTPUT file"""
+    
+    gevonden = False
+    
+    while not gevonden:
+        filename = input(prompt)
+        try:
+            file = open(filename, 'r')
+            if not filename.endswith(tuple(extentions)):
+                printlines()
+                print("note: this file has an unknown extention,\nit might not work properly.")
+                print("known formats are:",extentions)
+                printlines()
+            gevonden = True
+        except IOError or FileNotFoundError:
+            printlines()
+            print("Couldn't find the file named", filename)
+            print("make sure this file is located in the same\ndirectory from which this script is running")
+        except TypeError:
+            print("Wrong filetype")
+        else:
+            print("Thanks, found it!")
+        finally:
+            printlines()
+            
+    return file
+                    
+
+                
+
+def lees_inhoud(bestand):
+    """ - takes a FASTA file
+        - extracts headers and their sequences
+        - closes file
+        - returns headers, sequences
+
+        - INPUT  FASTA file type = (_io.TextIOWrapper)
+        - OUTPUT headers, sequences
+    """
     headers = []
-    seqs = []
-    
-    """
-    Schrijf hier je eigen code die het bestand inleest en deze splitst in headers en sequenties.
-    Lever twee lijsten op:
-        - headers = [] met daarin alle headers
-        - seqs = [] met daarin alle sequenties behorend bij de headers
-    Hieronder vind je de return nodig om deze twee lijsten op te leveren
-    """
-    
+    seqs = []    
     thissequence = ''
     for line in bestand:
         if ">" in line:
@@ -69,6 +106,8 @@ def lees_inhoud(bestands_naam):
         else:
             thissequence = thissequence + line.strip("\n")
     seqs.append(thissequence)
+
+    bestand.close()
     
     return headers, seqs
 
@@ -76,8 +115,8 @@ def lees_inhoud(bestands_naam):
 def is_dna(seq):
     """
     Deze functie bepaald of de sequentie (een element uit seqs) DNA is.
-    Indien ja, return True
-    Zo niet, return False
+    INPUT   indien ja, return True
+    OUTPUT  niet, return False
     """    
     for character in seq:
         if character not in ['A','T','C','G','n','N']:
@@ -86,36 +125,29 @@ def is_dna(seq):
     return True
 
 def get_enzyme_data(bestand):
-    """from afvink4
+    """
+    INPUT    a file containing enzyme-names and their 'cut'-location
+    OUTPUT   a list of tuples containing enzyme-names and their 'cut'-location
     """
     my_enzymes = []
-    bestand = open(bestand, 'r')
-    
-    for line in bestand: #iterate over every line in textfile 'bestand'
-        #if not line == '': # apparently I didn't even need this5
+    for line in bestand:    
         enzyme_name, cut_location = line.split() # split line at space, assign line[0] to name variable, line[1] to cut_location variable.
         my_enzymes.append((enzyme_name, cut_location, cut_location.replace("^",""))) # make a tuple with the data and append it to the my_enzyme list
+    bestand.close()
     return my_enzymes
     
 
 def knipt(sequence, list_of_enzymes):
     """
-    Bij deze functie kan je een deel van de code die je de afgelopen 2 afvinkopdrachten geschreven hebt herbruiken
-
-    Deze functie bepaald of een restrictie enzym in de sequentie (een element uit seqs) knipt.
-    Hiervoor mag je kiezen wat je returnt, of wellicht wil je alleen maar printjes maken.
+    
     """
     knip = False
     for enzyme in list_of_enzymes:
-        #print("checking for enzyme....",enzyme[0])
+
         if enzyme[2] in sequence:
             alignspot = sequence.find(enzyme[2])
             print(enzyme[0],"cuts at",alignspot)
             knip = True
-            
-            #graphical representation in shell (lol)
-            #print(sequence)
-            #print((alignspot-1)*' ',enzyme[2])
     print(knip)
 
        
